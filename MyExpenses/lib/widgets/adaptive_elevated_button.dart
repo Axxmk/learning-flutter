3a67408522d37269fa313ff889c17e5b56591b5e
Ann.mtr (MacBook Air) <monthara.k@mail.kmutt.ac.th>
2022-02-20 22:42:02 +0700

    [update] adaptive elevated button

---
+++ MyExpenses/lib/widgets/adaptive_elevated_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'dart:io';
+
+class AdaptiveElevatedButton extends StatelessWidget {
+  final String text;
+  final Function handler;
+
+  const AdaptiveElevatedButton({
+    Key key,
+    @required this.text,
+    @required this.handler,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Platform.isIOS
+        ? CupertinoButton(
+            color: Theme.of(context).colorScheme.primary,
+            child: Text(text),
+            onPressed: handler,
+          )
+        : ElevatedButton(
+            child: Text(text),
+            onPressed: handler,
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            ),
+          );
+  }
+}
